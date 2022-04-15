9673cb4b553120bddc32fd000d3f91623965dbcf
ruochen <wangrui@ruochen.email>
2022-04-15 15:14:27 +0800

    快速复制模块

--- springboot_01_01_quickstart/src/main/java/com/ruochen/Springboot0101QuickstartApplication.java
+++ springboot_01_01_quickstart/src/main/java/com/ruochen/Springboot0101QuickstartApplication.java
@@ -1,13 +1,17 @@
 package com.ruochen;
 
+import com.ruochen.Controller.BookController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Springboot0101QuickstartApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot0101QuickstartApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Springboot0101QuickstartApplication.class, args);
+        BookController bean = applicationContext.getBean(BookController.class);
+        System.out.println("bean => " + bean);
     }
 
 }
