e8e8aa78934e0cba10df37b3bbe56d9444e6c8e5
ruochen <wangrui@ruochen.email>
2022-04-15 22:24:18 +0800

    实体类快速开发---lombok

---
+++ springboot_ssmp/src/main/java/com/ruochen/domain/Book.java
@@ -0,0 +1,12 @@
+package com.ruochen.domain;
+
+import lombok.Data;
+
+// Lombok
+@Data
+public class Book {
+    private Integer id;
+    private String type;
+    private String name;
+    private String description;
+}
