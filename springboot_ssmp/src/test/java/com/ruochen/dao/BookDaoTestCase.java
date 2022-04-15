89119f53b2ce2c1b7e0b8431d23f1dcf0c0de4aa
ruochen <wangrui@ruochen.email>
2022-04-15 22:44:53 +0800

    crud

---
+++ springboot_ssmp/src/test/java/com/ruochen/dao/BookDaoTestCase.java
@@ -0,0 +1,57 @@
+package com.ruochen.dao;
+
+import com.ruochen.domain.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BookDaoTestCase {
+
+    @Autowired
+    private BookDao bookDao;
+
+    @Test
+    void testGetById() {
+        System.out.println(bookDao.selectById(1));
+    }
+
+    @Test
+    void testSave() {
+        Book book = new Book();
+        book.setType("测试数据");
+        book.setName("测试数据");
+        book.setDescription("测试数据");
+        bookDao.insert(book);
+    }
+
+    @Test
+    void testUpdate() {
+        Book book = new Book();
+        book.setId(13);
+        book.setType("测试数据aaa");
+        book.setName("测试数据aaa");
+        book.setDescription("测试数据");
+        bookDao.updateById(book);
+    }
+
+    @Test
+    void testDelete() {
+        bookDao.deleteById(13);
+    }
+
+    @Test
+    void testGetAll() {
+        System.out.println(bookDao.selectList(null));
+    }
+
+    @Test
+    void testGetPage() {
+
+    }
+
+    @Test
+    void testGetBy() {
+
+    }
+}
