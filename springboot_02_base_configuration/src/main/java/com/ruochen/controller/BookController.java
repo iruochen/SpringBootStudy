9213e06069dc052bbeb6c83053cdef7a64d992bc
ruochen <wangrui@ruochen.email>
2022-04-15 18:04:31 +0800

    yaml单一属性读取

--- springboot_02_base_configuration/src/main/java/com/ruochen/controller/BookController.java
+++ springboot_02_base_configuration/src/main/java/com/ruochen/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.ruochen.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/books")
 public class BookController {
 
+    // 读取yaml数据中的单一数据
+    @Value("${country}")
+    private String country1;
+
+    @Value("${user.name}")
+    private String name1;
+
+    @Value("${likes[1]}")
+    private String likes1;
+
     @GetMapping
     public String getById() {
         System.out.println("springboot is running ....");
+        System.out.println("country => " + country1);
+        System.out.println("name1 => " + name1);
+        System.out.println("likes1 => " + likes1);
         return "springboot is running ....";
     }
 }
