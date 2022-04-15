58c710be78cdc9017dafcf909d51964bca4a433e
ruochen <wangrui@ruochen.email>
2022-04-15 21:56:54 +0800

    springboot整合druid

--- springboot_04_integration/src/main/java/com/ruochen/dao/AccountDao.java
+++ springboot_04_integration/src/main/java/com/ruochen/dao/AccountDao.java
@@ -1,9 +1,12 @@
 package com.ruochen.dao;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.ruochen.domain.Account;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
-public interface AccountDao extends BaseMapper<Account> {
+public interface AccountDao {
+
+    @Select("select * from account where id = #{id}")
+    public Account getById(Integer id);
 }
