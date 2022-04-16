0d7ca030784a7709ee8d5a3b8c6bee08f075cebf
ruochen <wangrui@ruochen.email>
2022-04-16 11:17:40 +0800

    表现层开发

--- springboot_ssmp/src/main/java/com/ruochen/service/impl/BookServiceImpl.java
+++ springboot_ssmp/src/main/java/com/ruochen/service/impl/BookServiceImpl.java
@@ -1,11 +1,39 @@
 package com.ruochen.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ruochen.dao.BookDao;
 import com.ruochen.domain.Book;
 import com.ruochen.service.IBookService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImpl extends ServiceImpl<BookDao, Book> implements IBookService {
+
+    @Autowired
+    private BookDao bookDao;
+
+    @Override
+    public boolean saveBook(Book book) {
+        return bookDao.insert(book) > 0;
+    }
+
+    @Override
+    public boolean modify(Book book) {
+        return bookDao.updateById(book) > 0;
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        return bookDao.deleteById(id) > 0;
+    }
+
+    @Override
+    public IPage<Book> getPage(int currentPage, int pageSize) {
+        IPage page = new Page(currentPage, pageSize);
+        bookDao.selectPage(page, null);
+        return page;
+    }
 }
