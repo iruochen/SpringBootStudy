5e27c6aee20db189dd7572d03cbce6ec2f86cc60
ruochen <wangrui@ruochen.email>
2022-04-16 11:33:55 +0800

    表现层数据一致性处理

--- springboot_ssmp/src/main/java/com/ruochen/controller/BookController.java
+++ springboot_ssmp/src/main/java/com/ruochen/controller/BookController.java
@@ -1,6 +1,7 @@
 package com.ruochen.controller;
 
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.ruochen.controller.utils.R;
 import com.ruochen.domain.Book;
 import com.ruochen.service.IBookService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,32 +17,32 @@ public class BookController {
     private IBookService bookService;
 
     @GetMapping
-    public List<Book> getAll() {
-        return bookService.list();
+    public R getAll() {
+        return new R(true, bookService.list());
     }
 
     @PostMapping
-    public Boolean save(@RequestBody Book book) {
-        return bookService.save(book);
+    public R save(@RequestBody Book book) {
+        return new R(bookService.save(book));
     }
 
     @PutMapping
-    public Boolean update(@RequestBody Book book) {
-        return bookService.modify(book);
+    public R update(@RequestBody Book book) {
+        return new R(bookService.modify(book));
     }
 
     @DeleteMapping("{id}")
-    public Boolean delete(@PathVariable Integer id) {
-        return bookService.delete(id);
+    public R delete(@PathVariable Integer id) {
+        return new R(bookService.delete(id));
     }
 
     @GetMapping("{id}")
-    public Book getById(@PathVariable Integer id) {
-        return bookService.getById(id);
+    public R getById(@PathVariable Integer id) {
+        return new R(true, bookService.getById(id));
     }
 
     @GetMapping("{currentPage}/{pageSize}")
-    public IPage<Book> getPage(@PathVariable int currentPage, @PathVariable int pageSize) {
-        return bookService.getPage(currentPage, pageSize);
+    public R getPage(@PathVariable int currentPage, @PathVariable int pageSize) {
+        return new R(true, bookService.getPage(currentPage, pageSize));
     }
 }
