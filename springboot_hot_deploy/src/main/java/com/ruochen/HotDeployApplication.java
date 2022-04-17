e7a3ee29286a55fee88dc616523961e97ab5d955
ruochen <wangrui@ruochen.email>
2022-04-17 16:44:35 +0800

    关闭热部署功能

--- springboot_hot_deploy/src/main/java/com/ruochen/HotDeployApplication.java
+++ springboot_hot_deploy/src/main/java/com/ruochen/HotDeployApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HotDeployApplication{
+public class HotDeployApplication {
 
     public static void main(String[] args) {
+        // 可覆盖配置文件中关闭热部署配置
+        System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(HotDeployApplication.class, args);
     }
 
