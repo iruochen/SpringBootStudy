689b8ec83e1975607f77b97e3e30ac98eec97402
ruochen <wangrui@ruochen.email>
2022-04-17 17:53:50 +0800

    常用计量单位

--- springboot_configuration/src/main/java/com/ruochen/config/ServerConfig.java
+++ springboot_configuration/src/main/java/com/ruochen/config/ServerConfig.java
@@ -2,6 +2,13 @@ package com.ruochen.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.convert.DataSizeUnit;
+import org.springframework.boot.convert.DurationUnit;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 // @Component
 @Data
@@ -9,5 +16,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 public class ServerConfig {
     private String ipAddress;
     private int port;
+    // ms
     private long timeout;
+
+    // 默认为ms，可自定义单位
+    @DurationUnit(ChronoUnit.HOURS)
+    private Duration serverTimeOut;
+
+    // 存储容量大小，默认为 B
+    @DataSizeUnit(DataUnit.MEGABYTES)
+    private DataSize dataSize;
 }
