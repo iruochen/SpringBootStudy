2f5debf019d52c2c98e01aeb03eaa279362c5062
ruochen <wangrui@ruochen.email>
2022-04-17 17:31:34 +0800

    第三方bean属性绑定

---
+++ springboot_configuration/src/main/java/com/ruochen/SpringbootConfigurationApplication.java
@@ -0,0 +1,31 @@
+package com.ruochen;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.ruochen.config.ServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(ServerConfig.class)
+public class SpringbootConfigurationApplication {
+
+    @Bean
+    @ConfigurationProperties(prefix = "datasource")
+    public DruidDataSource dataSource() {
+        DruidDataSource ds = new DruidDataSource();
+        return ds;
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(SpringbootConfigurationApplication.class, args);
+        ServerConfig bean = ctx.getBean(ServerConfig.class);
+        System.out.println(bean);
+        DruidDataSource ds = ctx.getBean(DruidDataSource.class);
+        System.out.println(ds.getDriverClassName());
+    }
+
+}
