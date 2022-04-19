6f63dfd68c9580b45a9dbdc1ef29430bbbf3924d
ruochen <wangrui@ruochen.email>
2022-04-19 17:03:34 +0800

    虚拟请求状态匹配

--- springboot_test/src/test/java/com/ruochen/WebTest.java
+++ springboot_test/src/test/java/com/ruochen/WebTest.java
@@ -5,8 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcResultMatchersDsl;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.result.StatusResultMatchers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 // 开启虚拟MVC调用
@@ -24,5 +29,19 @@ public class WebTest {
         MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");
         // 执行对应请求
         mvc.perform(builder);
+    }
+
+    @Test
+    void testStatus(@Autowired MockMvc mvc) throws Exception {
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");
+        ResultActions action = mvc.perform(builder);
+
+        // 设定预期值，与真实值进行比较，成功测试通过，失败测试失败
+        // 定义本次调用的预期值
+        StatusResultMatchers status = MockMvcResultMatchers.status();
+        // 预计本次调用是成功的，状态200
+        ResultMatcher ok = status.isOk();
+        // 添加预期值到本次调用过程中进行匹配
+        action.andExpect(ok);
     }
 }
