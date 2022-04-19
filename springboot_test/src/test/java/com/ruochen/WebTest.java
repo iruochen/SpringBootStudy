b38fc5d181942f8449bac6a48c4053e730b8e00c
ruochen <wangrui@ruochen.email>
2022-04-19 16:56:54 +0800

    虚拟请求测试

--- springboot_test/src/test/java/com/ruochen/WebTest.java
+++ springboot_test/src/test/java/com/ruochen/WebTest.java
@@ -1,13 +1,28 @@
 package com.ruochen;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// 开启虚拟MVC调用
+@AutoConfigureMockMvc
 public class WebTest {
 
     @Test
     void test() {
+    }
 
+    @Test
+        // 注入虚拟MVC调用对象
+    void testWeb(@Autowired MockMvc mvc) throws Exception {
+        // 创建虚拟请求，当前访问 /books
+        MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.get("/books");
+        // 执行对应请求
+        mvc.perform(builder);
     }
 }
