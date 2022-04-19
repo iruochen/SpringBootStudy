63eb16d22a404dcf983fa8d05ab4070e150f3d2d
ruochen <wangrui@ruochen.email>
2022-04-19 19:27:41 +0800

    内置持久化解决方案---JdbcTemplate

--- springboot_sql/src/main/java/com/ruochen/dao/BookDao.java
+++ springboot_sql/src/main/java/com/ruochen/dao/BookDao.java
@@ -1,9 +1,5 @@
 package com.ruochen.dao;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ruochen.domain.Book;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BookDao extends BaseMapper<Book> {
+// @Mapper
+public interface BookDao {
 }
