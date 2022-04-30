5ff1efe2ecbac4b653662351ac2754d0cff866dc
ruochen <wangrui@ruochen.email>
2022-04-30 14:30:01 +0800

    springboot整合redis

---
+++ springboot_redis/src/test/java/com/ruochen/SpringbootRedisApplicationTests.java
@@ -0,0 +1,27 @@
+package com.ruochen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+@SpringBootTest
+class SpringbootRedisApplicationTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    void set() {
+        ValueOperations ops = redisTemplate.opsForValue();
+        ops.set("age", 22);
+    }
+
+    @Test
+    void get() {
+        ValueOperations ops = redisTemplate.opsForValue();
+        Object age = ops.get("age");
+        System.out.println(age);
+    }
+}
